0a3bd20759956763ad9235f18f43f8ffe66fffc8
orbtlzz <temelak0912@gmail.com>
2026-05-18 14:32:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -34,10 +34,16 @@ app.use(express.json());
 // 5. EMAIL_USER=your.address@gmail.com
 // ─────────────────────────────────────────────────────
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  connectionTimeout: 30000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
