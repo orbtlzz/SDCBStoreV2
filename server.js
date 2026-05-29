93681a460fef40d6c7d34f387fac80ffc0e9aecd
orbtlzz <temelak0912@gmail.com>
2026-05-29 12:35:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   connectionTimeout: 30000,
+  family: 4,                          // ← add this
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
